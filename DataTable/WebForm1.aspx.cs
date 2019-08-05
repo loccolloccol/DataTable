e638cb88f361c26e462f9f23e2371122d8b89571
江龍 卓 <loccolloccol@yahoo.com.tw>
2019-08-05 11:44:46 +0800

    加入專案檔案。

---
+++ DataTable/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataTable
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            dt.Columns.Add("Name",typeof(string));
+            dt.Columns.Add("Age", typeof(Int16));
+            dt.Columns.Add("ADDR", typeof(string));
+            dt.Columns.Add("MOBILE", typeof(string));
+            dt.Columns.Add("Birtyday", typeof(DateTime));
+        }
+    }
+}
